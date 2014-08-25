7ee74f9abdfe0f81758961e497954d5583bc5718
Ben Schmeckpeper <ben@aisle50.com>
2014-08-25 14:58:29 -0500

    Focus the textarea after double clicking a cell

--- app/assets/javascripts/week.js
+++ app/assets/javascripts/week.js
@@ -4,8 +4,10 @@ Week = function() {
     self.init = function() {
         $(".day").on("dblclick", function(event) {
             var $cell = $(this);
-            if ($cell.find("ul:visible").length > 0)
+            if ($cell.find("ul:visible").length > 0) {
                 self.toggle($cell);
+                $cell.find("textarea").focus();
+            }
             return false;
         });
         $(".btn-danger").on("click", function(event) {
