e7b7db171503846e8ac749e322b57c6f00f1419a
Ben Schmeckpeper <ben@aisle50.com>
2014-08-25 13:45:45 -0500

    Methods to create and populate Year, Week, Day.

--- app/models/week.rb
+++ app/models/week.rb
@@ -2,4 +2,13 @@ class Week < ActiveRecord::Base
   belongs_to :year
 
   has_many :days, dependent: :destroy
+
+  def self.populate(number, start_date)
+    week = create(number: number, start_date: start_date)
+    %w{ Monday Tuesday Wednesday Thursday Friday}.each_with_index do |day, i|
+      week.days.create(name: day, date: start_date + i.days)
+    end
+
+    week
+  end
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -1,3 +1,12 @@
 class Year < ActiveRecord::Base
   has_many :weeks, dependent: :destroy
+
+  def self.populate(name, number_of_weeks, start_date)
+    year = create(name: name)
+    0.upto(number_of_weeks) do |i|
+      year.weeks << Week.populate(i+1, start_date + i.weeks)
+    end
+
+    year
+  end
 end
