2ee4c5f279594641508eb5dda0fe062bb4d2b1b8
Ben Schmeckpeper <ben@aisle50.com>
2014-08-25 13:46:02 -0500

    Get items by grade.

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,4 +2,8 @@ class Day < ActiveRecord::Base
   belongs_to :week
 
   has_many :items, dependent: :destroy
+
+  def items_for_grade(grade)
+    items.for_grade(grade)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :day
+
+  scope :for_grade, ->(grade){ where(grade: grade) }
 end
