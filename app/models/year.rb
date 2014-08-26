79c60aef9621d12ca1b30d9a89224df95635ca13
Ben Schmeckpeper <ben@aisle50.com>
2014-08-26 14:13:08 -0500

    Add a method to extend a year by adding more weeks.

--- app/models/year.rb
+++ app/models/year.rb
@@ -7,10 +7,20 @@ class Year < ActiveRecord::Base
 
   def self.populate(name, number_of_weeks, start_date)
     year = create(name: name)
-    0.upto(number_of_weeks) do |i|
+    number_of_weeks.times do |i|
       year.weeks << Week.populate(i+1, start_date + i.weeks)
     end
 
     year
+  end
+
+  def extend(number_of_weeks)
+    last_week = weeks.order(:number).last
+    start_number = last_week.number + 1
+    start_date = last_week.start_date + 1.week
+
+    number_of_weeks.times do |i|
+      weeks << Week.populate(start_number + i, start_date + i.weeks)
+    end
   end
 end
