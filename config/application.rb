8cdfc1dea090768a1c7facef5b37b0d9bd9914ef
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2021-10-26 07:14:48 -0500

    Don't halt callback chains on false

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Calendar
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    ActiveSupport.halt_callback_chains_on_return_false = false
   end
 end
