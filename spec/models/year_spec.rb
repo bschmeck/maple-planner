cc8d11ed01fec1d1c2457edddaf628e86068d94c
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2026-04-16 20:50:23 -0500

    Add specs for creating and extending a year

--- spec/models/year_spec.rb
+++ spec/models/year_spec.rb
@@ -23,4 +23,30 @@ describe Year do
       expect(week.next_week.start_date).to be >= Date.today
     end
   end
+
+  describe "#extend" do
+    it "creates additional weeks" do
+      user = create(:user)
+      year = Year.populate(user, "Test", 10, 1.month.ago)
+      expect { year.extend(2) }.to change(Week, :count).by(2)
+    end
+  end
+
+  describe "::populate" do
+    it "creates a Year" do
+      user = create(:user)
+      expect { Year.populate(user, "Test", 10, 1.month.ago) }.to change(Year, :count).by(1)
+    end
+
+    it "creates weeks for the new year" do
+      user = create(:user)
+      year = Year.populate(user, "Test", 10, 1.month.ago)
+      expect(year.weeks.count).to eq 10
+    end
+
+    it "creates days for each week" do
+      user = create(:user)
+      expect { Year.populate(user, "Test", 10, 1.month.ago) }.to change(Day, :count).by(50)
+    end
+  end
 end
